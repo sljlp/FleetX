823a46ed4b6acb914f87bfb8c1bc1348479138b1
liupeng51 <liupeng51@baidu.com>
2021-06-30 23:49:00 +0800

    update draw loss for drawing loss scaling

--- examples/hybrid_parallelism/draw_loss.py
+++ examples/hybrid_parallelism/draw_loss.py
@@ -11,26 +11,30 @@ def parse_line(line):
     
     step_idx, cost_idx, loss_idx, sen_idx = line.find('step:'), line.find(', cost:'), line.find(', mlm loss:'), line.find(last_key)
     '''learning rate: 1.617e-05, loss_scalings: '''
-    lr_idx, lsscale_idx = line.find('learning rate: '), line.find(', loss_scalings:')
+    lr_idx, lsscale_idx, pploss_idx = line.find('learning rate: '), line.find(', loss_scalings:'), line.find(", pp_loss:")
     step = int(line[step_idx + 5 : cost_idx])
     cost = float(line[cost_idx + 7 : loss_idx])
     loss = float(line[loss_idx + 11 : sen_idx])
     lr = float(line[lr_idx + 14 : lsscale_idx])
-    return step, cost, loss, lr
+    loss_scaling = float(line[lsscale_idx + 16: pploss_idx])
+    # print(loss_scaling)
+    return step, cost, loss, lr, loss_scaling
 
 
 c1, c2 = None, None
 cc = 0
 clen = 0
 for name in sys.argv[1:]:
-    step, cost, loss, lrs = [], [], [], [] 
+    step, cost, loss, lrs, ls_scale = [], [], [], [], [] 
     for line in open(name):
         if 'step' in line and 'cost' in line and 'loss' in line and 'learning rate' in line:
-            s, c, l, lr = parse_line(line)
+            s, c, l, lr, lss = parse_line(line)
             step.append(s)
             cost.append(c)
             loss.append(l)
             lrs.append(lr)
+            # print(lss)
+            ls_scale.append(lss)
     plt.figure('align cost')
     plt.plot(step, cost, label=name)
     plt.legend(loc=0,)
@@ -44,6 +48,12 @@ for name in sys.argv[1:]:
     plt.figure('align lr')
     plt.plot(step, lrs, label=name)
     plt.legend(loc=0,)
+
+    plt.figure("ls scaling")
+    # print(ls_scale)
+    plt.plot(step, ls_scale, label=name)
+    plt.legend(loc=0,)
+
 clen = min(len(c1), len(c2))
 c1 = c1[:clen]
 c2 = c2[:clen]
