4ed88dd573c9e780047f6fbc024cf68d761b0643
sljlp <lrslj@live.com>
2021-12-14 11:01:12 +0800

    baseline on gpu

--- examples/resnet/run.sh
+++ examples/resnet/run.sh
@@ -1,3 +1,8 @@
+export PYTHONPATH=/code_lp/paddle/Paddle/build/python/:
+export FLAGS_cudnn_deterministic=True
+export CUDA_VISIBLE_DEVICES=4
+LOG=${LOG:-log}
 python3 -m paddle.distributed.fleet.launch \
+--log_dir $LOG \
 --run_mode="collective" \
 train_fleet_dygraph.py
