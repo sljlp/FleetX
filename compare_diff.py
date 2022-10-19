e26ea06b63646581e29c8805aa6f4a7b207eb475
sljlp <lrslj@live.com>
2022-10-19 10:46:28 +0800

    compare

---
+++ compare_diff.py
@@ -0,0 +1,51 @@
+import pickle
+import numpy as np
+import copy
+
+def strip(data):
+    assert isinstance(data, dict)
+    data.pop("StructuredToParameterName@@", None)
+
+def load_params(path):
+    with open(path, "rb") as f:
+        data = pickle.load(f, encoding='latin1')
+    return data
+
+def parse_rank_auto(dp, mp, pp):
+    ranks = list(range(8))
+    ranks = ranks[pp*4:pp*4+4]
+    ranks = ranks[dp*2:dp*2+2]
+    rank = ranks[mp]
+    return rank
+
+def test(strategy):
+    mp1 = parse_rank_auto(*strategy)
+    mp_model = load_params(f"before_train/static_saved_dist{mp1}.pdparams")
+
+    pp0_static = mp_model
+
+    mp1 = parse_rank_dygraph(*strategy)
+    mp_model_dy = load_params(f"output_dp2mp2pp2/auto_dist{mp1}.pdparams")
+
+    pp0_dygraph = mp_model_dy
+
+    for k in pp0_static.keys():
+        if k in pp0_dygraph:
+            d1 = pp0_static[k]
+            d2 = pp0_dygraph[k]
+            assert np.allclose(d1, d2)
+        else:
+            print("not matched key:", k)
+
+def parse_rank_dygraph(dp, mp, pp):
+    ranks = list(range(4))
+    ranks = ranks[pp*2:pp*2+2]
+    rank = ranks[mp]
+    return rank
+
+if __name__ == "__main__":
+
+    model = load_params("output_dp2mp4/epoch_0_step_0/mp_00_sharding_00_pp_00/dist_saved_dist0.pdparams")
+    attr = load_params("output_dp2mp4/epoch_0_step_0/mp_00_sharding_00_pp_00/dist_saved_dist0.pdattr")
+
+    attr = load_params("")
